06dc17e0c4dc8155c44bd09c5f938577ccef4ac4
AndrewTechTips <andreww.steel69@gmail.com>
2026-05-12 15:52:58 +0300

    Refactor Hotel class and improve booking logic

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+
+        return availability == "yes"
 
 
 class ReservationTicket:
@@ -22,8 +27,8 @@ class ReservationTicket:
         pass
 
 
-id = input("Enter the id of the hotel: ")
-hotel = Hotel(id)
+hotel_id = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_id)
 
 if hotel.available():
     hotel.book()
